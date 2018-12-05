223066e7846b1243dd3903f6cd7dba4ee573ddf0
elamami <elalami2>
2018-12-05 17:15:17 +0100

    les liens

--- app/javascript/packs/adress_auto.js
+++ app/javascript/packs/adress_auto.js
@@ -1,15 +1,15 @@
-import 'mapbox-gl/dist/mapbox-gl.css'
-import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
-import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
-import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+// import 'mapbox-gl/dist/mapbox-gl.css'
+// import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
+// import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
+// import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 
 
-// const addressInput = document.getElementById('user_address');
+const addressInput = document.getElementById('user_address');
 
-// if (addressInput) {
-//   const places = require('places.js');
-//   const placesAutocomplete = places({
-//     container: addressInput
-//   });
-// }
+if (addressInput) {
+  const places = require('places.js');
+  const placesAutocomplete = places({
+    container: addressInput
+  });
+}
